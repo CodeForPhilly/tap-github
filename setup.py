b10da1df427330b552856ebcc60a7c1c0d0836de
Darrel Herbst <dherbst@gmail.com>
2021-02-07 09:51:41 -0500

    Remove black formatting fix

--- setup.py
+++ setup.py
@@ -2,27 +2,31 @@
 
 from setuptools import setup, find_packages
 
-setup(
-    name="tap-github",
-    version="1.9.0.cfp",
-    description="Singer.io tap for extracting data from the GitHub API",
-    author="CodeForPhilly",
-    url="https://codeforphilly.org",
-    classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["tap_github"],
-    install_requires=["singer-python==5.9.1", "requests==2.25.1"],
-    extras_require={
-        "dev": [
-            "pylint",
-            "ipdb",
-            "nose",
-        ]
-    },
-    entry_points="""
+setup(name='tap-github',
+      version='1.9.1',
+      description='Singer.io tap for extracting data from the GitHub API',
+      author='Stitch',
+      url='http://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_github'],
+      install_requires=[
+          'singer-python==5.9.1',
+          'requests==2.25.1'
+      ],
+      extras_require={
+          'dev': [
+              'pylint',
+              'ipdb',
+              'nose',
+          ]
+      },
+      entry_points='''
           [console_scripts]
           tap-github=tap_github:main
-      """,
-    packages=["tap_github"],
-    package_data={"tap_github": ["tap_github/schemas/*.json"]},
-    include_package_data=True,
+      ''',
+      packages=['tap_github'],
+      package_data = {
+          'tap_github': ['tap_github/schemas/*.json']
+      },
+      include_package_data=True
 )
